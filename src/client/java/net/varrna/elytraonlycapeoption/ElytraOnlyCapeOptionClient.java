49d4c77bf94158b994585f96ad85302ea35e62f6
vilius.pas <vilius.paskonis@gmail.com>
2024-07-11 17:25:15 +0300

    bug fix: capeOption is null

--- src/client/java/net/varrna/elytraonlycapeoption/ElytraOnlyCapeOptionClient.java
+++ src/client/java/net/varrna/elytraonlycapeoption/ElytraOnlyCapeOptionClient.java
@@ -13,7 +13,7 @@ import net.minecraft.network.message.ChatVisibility;
 import java.util.Arrays;
 
 public class ElytraOnlyCapeOptionClient implements ClientModInitializer {
-	private static CapeOption capeOption;
+	private static CapeOption capeOption = CapeOption.ELYTRA_ONLY;
 	@Override
 	public void onInitializeClient() {
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
