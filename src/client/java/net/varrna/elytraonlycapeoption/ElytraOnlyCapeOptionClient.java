aaeda465497906fc93a1ad54932a87ab2a9e6464
vilius.pas <vilius.paskonis@gmail.com>
2024-07-11 21:24:09 +0300

    added save cape option cache

--- src/client/java/net/varrna/elytraonlycapeoption/ElytraOnlyCapeOptionClient.java
+++ src/client/java/net/varrna/elytraonlycapeoption/ElytraOnlyCapeOptionClient.java
@@ -1,21 +1,23 @@
 package net.varrna.elytraonlycapeoption;
 
-import com.mojang.serialization.Codec;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.SimpleOption;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerModelPart;
 import net.minecraft.item.Items;
-import net.minecraft.network.message.ChatVisibility;
-
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ElytraOnlyCapeOptionClient implements ClientModInitializer {
+	private static final Path CONFIG_PATH = Paths.get("config", "elytraonlycapeoption.txt");
 	private static CapeOption capeOption = CapeOption.ELYTRA_ONLY;
 	@Override
 	public void onInitializeClient() {
+		loadCapeOption();
+
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (client.player != null) {
 				switch (capeOption) {
@@ -45,9 +47,26 @@ public class ElytraOnlyCapeOptionClient implements ClientModInitializer {
 
 	public static void setCapeOption(CapeOption option) {
 		capeOption = option;
+		saveCapeOption();
 	}
 
 	public static CapeOption getCapeOption() {
 		return capeOption;
+	}
+
+	private static void saveCapeOption() {
+		try {
+			Files.createDirectories(CONFIG_PATH.getParent());
+			Files.writeString(CONFIG_PATH, capeOption.name());
+		} catch (IOException ignore) {}
+	}
+
+	private static void loadCapeOption() {
+		if (Files.exists(CONFIG_PATH)) {
+			try {
+				String optionName = Files.readString(CONFIG_PATH).trim();
+				capeOption = CapeOption.valueOf(optionName);
+			} catch (IOException | IllegalArgumentException ignore) {}
+		}
 	}
 }
